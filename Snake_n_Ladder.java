794ceef4710914a26901aa0982f842c14a5feeb2
smutallib97 <smutallib97@gmail.com>
2022-09-24 23:45:26 +0530

    Starting Possition of the Player

--- Snake_n_Ladder.java
+++ Snake_n_Ladder.java
@@ -2,6 +2,11 @@ package com.bridgelabz.snake_n_ladder;
 
 public class Snake_n_Ladder {
     public static void main(String[] args) {
-        System.out.println("Welcome to Snake_N_Ladder Computation Program");
+        //System.out.println("Welcome to Snake_N_Ladder Computation Program");
+        //UC1
+
+        // starting position of player
+        int startPosition = 0;
+        System.out.println(" Start position is: " + startPosition);
     }
 }
