dec5ec97814c8c7cb2b889fb4fca578702155daf
smutallib97 <smutallib97@gmail.com>
2022-09-24 23:33:09 +0530

    Welcome to SnakeNLadder Computation Program

---
+++ Snake_n_Ladder.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.snake_n_ladder;
+
+public class Snake_n_Ladder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake_N_Ladder Computation Program");
+    }
+}
