6f2b32323f51dbf6520461e4db806573af4b41af
smutallib97 <smutallib97@gmail.com>
2022-09-25 00:06:30 +0530

    Check For a Option

--- Snake_n_Ladder.java
+++ Snake_n_Ladder.java
@@ -12,5 +12,26 @@ public class Snake_n_Ladder {
         //UC2
         int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
         System.out.println("Dice Number is : " + diceNumber);
+
+        // to check option
+        int option = (int) Math.floor(Math.random() * 10) % 3;
+        System.out.println("Player option is : " + option);
+
+        switch (option) {
+            case 1:
+                startPosition = diceNumber + startPosition;
+                System.out.println("Ladder");
+                System.out.println("Position for the player after the ladder is : " + startPosition);
+                break;
+
+            case 2:
+                startPosition = startPosition - diceNumber;
+                System.out.println("Snack");
+                System.out.println("Position for the player after the Snake is : " + startPosition);
+                break;
+
+            default:
+                System.out.println("No play");
+        }
     }
 }
